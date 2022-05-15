aff89f925186631f3b62205f57008ad2e42a12d0
Nijin P S <nijingithub@gmail.com>
2022-05-15 17:48:52 +0530

    Add RedisController class

--- src/main/java/com/example/Redis_Spring/RedisConfig.java
+++ src/main/java/com/example/Redis_Spring/RedisConfig.java
@@ -1,4 +1,24 @@
 package com.example.Redis_Spring;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+
+@Configuration
 public class RedisConfig {
+
+    public JedisConnectionFactory getRedisFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+                "redis-18706.c212.ap-south-1-1.ec2.cloud.redislabs.com", 18706);
+        redisStandaloneConfiguration.setPassword("F1lu42k3nieD99ViIywMn51nynYLt95i");
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    public RedisTemplate<String, Object> getTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setValueSerializer(JdkSerializationRedisSerializer());
+    }
 }
